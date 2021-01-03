0235b4b226c2697435166859add458acceb8c589
SergeyTrushin <sergeytrushin-17@yandex.ru>
2021-01-03 13:11:38 +0400

    refact

--- src/App.vue
+++ src/App.vue
@@ -1,309 +1,301 @@
 <template>
-  <div id="app" v-responsive="{ small: el => el.width < 720 }">
-	<div id="container" ref='con'>
-		
-
-		<div id="core-toolbar">
-			<p class="rate">Курс {{ baseCurrency }} сегодня</p>
-
-			<nav class="tabs">
-					<button class="left" 
-							v-if="activeCur!==0" 
-							@click="previous">
-					</button>
-
-					<button class="tab" 
-							v-for="p,i in paginatedData"
-							v-bind:class="{ active : isActive(p) }"
-							@click="getNumber(i,p)">
-							
-							{{ p }}
-					
-					</button>
-
-					<button class="right" 
-							v-if="activeCur !== getListOfRates.length-1" 
-							@click="next">
-					</button>
-			</nav>
-
-		</div>
-		
-		<div class="input">
-			<input type="tel"
-				   @input='clear'
-				   v-model:value="value"> <span>{{ baseCurrency }} </span>
-		</div>
-
-		<Cards v-responsive="{ small: el => el.width < 720 }"
-			   :value="value"
-			   :size="size"
-		/>
-
-	</div>
+  <div id="app" v-responsive="{ small: (el) => el.width < 720 }">
+    <div id="container" ref="con">
+      <div id="core-toolbar">
+        <p class="rate">Курс {{ baseCurrency }} сегодня</p>
+        <nav class="tabs">
+          <button
+            class="left"
+            v-if="activeCur !== 0"
+            @click="previous"
+          ></button>
+          <button
+            class="tab"
+            v-for="(p, i) in paginatedData"
+            v-bind:class="{ active: isActive(p) }"
+            @click="getNumber(i, p)"
+          >
+            {{ p }}
+          </button>
+          <button
+            class="right"
+            v-if="activeCur !== getListOfRates.length - 1"
+            @click="next"
+          ></button>
+        </nav>
+      </div>
+      <div class="input">
+        <input type="tel" @input="clear" v-model:value="value" />
+        <span>{{ baseCurrency }} </span>
+      </div>
+      <Cards
+        v-responsive="{ small: (el) => el.width < 720 }"
+        :value="value"
+        :size="size"
+      />
+    </div>
   </div>
 </template>
 
 
 <script>
-
-import Cards from '@/components/Cards.vue';
+import Cards from "@/components/Cards.vue";
 import { ResponsiveDirective } from "vue-responsive-components";
 
 export default {
-	components : {
-		Cards
-	},
-	directives: {
-    	responsive: ResponsiveDirective
-  	},
-	data(){
-		return{
-			value : '1', // Значение в input
-			activeCur : 0,  // индекс активной валюты, которая конвертируется
-			pageNumber: 0, // номер страницы с валютами
-			show: 7, // Количество валют на одной странице
-			size : 4 // Количество блоков с переводом в другие валюты
-		}
-	},
-	computed: {
-		baseCurrency(){
-			return this.$store.getters.getBaseCurrency
-		},
-		getListOfRates(){
-			return this.$store.getters.getListOfRates
-		},
-		getRates(){
-			return this.$store.getters.getRates
-		},
-		pageCount(){
-			let l = this.getListOfRates.length,
-          		s = this.end;
-        	return Math.ceil(l/s);
-		},
-		paginatedData(){
-    		const start = this.pageNumber * this.show,
-          		  end = start + this.show;
-    	return this.getListOfRates.slice(start, end);
-		},
+  components: {
+    Cards,
+  },
+  directives: {
+    responsive: ResponsiveDirective,
+  },
+  data() {
+    return {
+      value: "1", // Значение в input
+      activeCur: 0, // индекс активной валюты, которая конвертируется
+      pageNumber: 0, // номер страницы с валютами
+      show: 7, // Количество валют на одной странице
+      size: 4, // Количество блоков с переводом в другие валюты
+    };
+  },
+  computed: {
+    baseCurrency() {
+      return this.$store.getters.getBaseCurrency;
+    },
+    getListOfRates() {
+      return this.$store.getters.getListOfRates;
+    },
+    getRates() {
+      return this.$store.getters.getRates;
+    },
+    pageCount() {
+      let l = this.getListOfRates.length,
+        s = this.end;
+      return Math.ceil(l / s);
+    },
+    paginatedData() {
+      const start = this.pageNumber * this.show,
+        end = start + this.show;
+      return this.getListOfRates.slice(start, end);
+    },
+  },
+  async mounted() {
+    this.$store.dispatch("fetchData");
 
-	},
-	async mounted(){
-		this.$store.dispatch("fetchData")
+    window.onresize = () => {
+      if (this.$refs.con.clientWidth < 720) {
+        this.show = 3;
+        this.size = 2;
+        this.pageNumber = 0;
+        while (this.pageNumber * this.show + this.show <= this.activeCur) {
+          this.pageNumber++;
+        }
+      } else {
+        this.show = 7;
+        this.size = 4;
+        this.pageNumber = 0;
+        while (this.pageNumber * this.show + this.show <= this.activeCur) {
+          this.pageNumber++;
+        }
+      }
+    };
 
-		window.onresize = ()=>{
+    window.onload = () => {
+      this.activeCur = this.$store.getters.getActive;
+      if (this.$refs.con.clientWidth < 720) {
+        this.show = 3;
+        this.size = 2;
+        this.pageNumber = 0;
+        while (this.pageNumber * this.show + this.show <= this.activeCur) {
+          this.pageNumber++;
+        }
+      } else {
+        this.show = 7;
+        this.size = 4;
+        this.pageNumber = 0;
+        while (this.pageNumber * this.show + this.show <= this.activeCur) {
+          this.pageNumber++;
+        }
+      }
+    };
+  },
+  methods: {
+    clear() {
+      this.value = this.value.replace(/[^\d.]/g, "").replace(/^0\d.*/g, "");
+    },
 
-			if (this.$refs.con.clientWidth<720){
-				this.show = 3
-				this.size = 2
-				this.pageNumber = 0;
-				while (this.pageNumber * this.show + this.show <= this.activeCur){
-					this.pageNumber++	
-				}
-			}else {
-				this.show = 7
-				this.size = 4
-				this.pageNumber = 0;
-				while (this.pageNumber * this.show + this.show <= this.activeCur){
-					this.pageNumber++	
-				}
-			}
-		}
+    getNumber(i, p) {
+      this.$store.dispatch("updateBase", p);
+      this.activeCur = this.getListOfRates.indexOf(p);
+    },
 
-		window.onload = ()=>{
-			this.activeCur = this.$store.getters.getActive
-			if (this.$refs.con.clientWidth<720){
-				this.show = 3
-				this.size = 2
-				this.pageNumber = 0;
-				while (this.pageNumber * this.show + this.show <= this.activeCur){
-					this.pageNumber++	
-				}
-			}else {
-				this.show = 7
-				this.size = 4
-				this.pageNumber = 0;
-				while (this.pageNumber * this.show + this.show <= this.activeCur){
-					this.pageNumber++	
-				}
-			}
-		}
-	},
-	methods: {
-		clear(){
-			this.value = this.value.replace(/[^\d.]/g,'').replace(/^0\d.*/g,"")
-			
-		},
+    next() {
+      if (
+        this.paginatedData[this.paginatedData.length - 1] ===
+        this.getListOfRates[this.activeCur]
+      ) {
+        this.pageNumber++;
+      }
 
-		getNumber(i,p){
-			this.$store.dispatch('updateBase', p)
-			this.activeCur = this.getListOfRates.indexOf(p)
-		},
+      if (this.activeCur < this.getListOfRates.length - 1) {
+        this.activeCur++;
+        this.$store.dispatch("updateBase", this.getListOfRates[this.activeCur]);
+      }
+    },
 
-		next(){
-			if(this.paginatedData[this.paginatedData.length-1] === this.getListOfRates[this.activeCur] ){
-				this.pageNumber++;
-			}
+    previous() {
+      if (this.paginatedData[0] === this.getListOfRates[this.activeCur]) {
+        this.pageNumber--;
+      }
 
-			if(this.activeCur < this.getListOfRates.length-1){
-				this.activeCur++;
-				this.$store.dispatch('updateBase', this.getListOfRates[this.activeCur])
-			}
-		},
-
-		previous(){
-			if(this.paginatedData[0] === this.getListOfRates[this.activeCur] ){
-				this.pageNumber--;
-			}
-
-			if(this.activeCur > 0){
-				this.activeCur--;
-				this.$store.dispatch('updateBase', this.getListOfRates[this.activeCur])
-			}
-		},
-		isActive(cur){
-			return cur === this.getListOfRates[this.activeCur]
-		},
-	}
-}
+      if (this.activeCur > 0) {
+        this.activeCur--;
+        this.$store.dispatch("updateBase", this.getListOfRates[this.activeCur]);
+      }
+    },
+    isActive(cur) {
+      return cur === this.getListOfRates[this.activeCur];
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-	$navColor : #FFE782;
+$navColor: #ffe782;
 
-	@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
 
-	body{
-		font-family: 'Roboto', sans-serif;
-	}
-	
-	*{
-		box-sizing: border-box;
-	}
+body {
+  font-family: "Roboto", sans-serif;
+}
 
-	button{
-		&:active{
-				outline : none;
-		}
-		&:focus{
-				outline: none;
-		}
-	}
+* {
+  box-sizing: border-box;
+}
 
-	#app{
-		#container{
-			margin: 0 auto;
-			max-width: 720px;
-			background: #fff;
-			padding-bottom: 24px;
-		}
-	}
+button {
+  &:active {
+    outline: none;
+  }
+  &:focus {
+    outline: none;
+  }
+}
 
-	#app.small{
-		#container{
-			width: 320px;
-			.left{
-				left: -10px;
-			}
+#app {
+  #container {
+    margin: 0 auto;
+    max-width: 720px;
+    background: #fff;
+    padding-bottom: 24px;
+  }
+}
 
-			.right{
-				right: -10px;
-			}
+#app.small {
+  #container {
+    width: 320px;
+    .left {
+      left: -10px;
+    }
 
-			.tab{
-				width: 80px;
-			}
-		}
-	}
+    .right {
+      right: -10px;
+    }
 
-	#core-toolbar{
-		background: $navColor;
-		padding: 1rem 1.5rem 0 1.5rem;
-			
-		.rate{
-			font-weight: normal;
-			font-size: 21px;
-			line-height: 25px;
-			margin-bottom: 8px;
-		}
+    .tab {
+      width: 80px;
+    }
+  }
+}
 
-		.tabs{
-			display: flex;
-			justify-content: center;
-			position: relative;
-			.tab{
-				position: relative;
-				top: 5px;
-				padding: 0;
-				width: 88px; 
-				height: 48px;
-				font-weight: 500;
-				font-size: 14px;
-				line-height: 16px;
-				text-align: center;
-				background: none;
-				border: none;
-				color: #CCAE68;
-			}
-			.left,.right{
-				color: #CCAE68;
-				padding: 0;
-				margin: 0;
-				position: absolute;
-				content: "a";
-				width: 8px;
-				height: 8px;
-				background: none;
-				border: none;
-				border-top: 1px solid;
-				border-left: 1px solid;
-			}
+#core-toolbar {
+  background: $navColor;
+  padding: 1rem 1.5rem 0 1.5rem;
 
-			.left{
-				top:50%;
-				left: -5px;
-				transform: rotate(-45deg);
-			}
-			.right{
-				top: 50%;
-				right: -5px;
-				transform: rotate(135deg);
-			}
-			.active{
-				background: #fff;
-				border-radius: 5px;
-				color: #000;
-				font-weight: bold;
-			}
-		}
-	}
+  .rate {
+    font-weight: normal;
+    font-size: 21px;
+    line-height: 25px;
+    margin-bottom: 8px;
+  }
 
-	.input{
-		margin: 29px 35px 23px 0;
-		display: flex;
-		justify-content: flex-end;
-		align-items: center;
-		input{
-			border-radius: none;
-			border: none;
-			border-bottom: 1px solid #D9D9D9;
-			padding: 0.5rem;
-			width: 158px;
-			outline: none;
-			text-align: end;
-			margin-right: .5rem;
-			font-weight: normal;
-			font-size: 18px;
-			line-height: 21px;
-			color: #2B2D33;
-		}
+  .tabs {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    .tab {
+      position: relative;
+      top: 5px;
+      padding: 0;
+      width: 88px;
+      height: 48px;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 16px;
+      text-align: center;
+      background: none;
+      border: none;
+      color: #ccae68;
+    }
+    .left,
+    .right {
+      color: #ccae68;
+      padding: 0;
+      margin: 0;
+      position: absolute;
+      content: "a";
+      width: 8px;
+      height: 8px;
+      background: none;
+      border: none;
+      border-top: 1px solid;
+      border-left: 1px solid;
+    }
 
-		span{
-			font-weight: normal;
-			font-size: 18px;
-			line-height: 21px;
-			color: #B9B9B9;
-		}
-	}
-	
+    .left {
+      top: 50%;
+      left: -5px;
+      transform: rotate(-45deg);
+    }
+    .right {
+      top: 50%;
+      right: -5px;
+      transform: rotate(135deg);
+    }
+    .active {
+      background: #fff;
+      border-radius: 5px;
+      color: #000;
+      font-weight: bold;
+    }
+  }
+}
+
+.input {
+  margin: 29px 35px 23px 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  input {
+    border-radius: none;
+    border: none;
+    border-bottom: 1px solid #d9d9d9;
+    padding: 0.5rem;
+    width: 158px;
+    outline: none;
+    text-align: end;
+    margin-right: 0.5rem;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 21px;
+    color: #2b2d33;
+  }
+
+  span {
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 21px;
+    color: #b9b9b9;
+  }
+}
 </style>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,224 +1,224 @@
 <template>
-	<div id='main'>
-		<div class="items" ref='items'>
-			
-			<div v-if="!getRates" class="lds-dual-ring"></div>
-			<div v-else class="item" v-for="curName of paginatedData">
-				<p class="curName">{{ value ? value : 0 }}  <span>{{ baseCurrency }} = </span></p>
-				<div class="curValue">
-					<span class="valueOfCur">
-						{{ calc(getRates[curName]).replace(/\./g,',') !== 'NaN' ? calc(getRates[curName]).replace(/\./g,',') : '0' }}
-					</span> 
-
-					<span class="curName">
-						{{ curName }}
-					</span>
-
-				</div>
-			
-			</div> 
-		</div>
-
-		<div class="btn">
-			<button class="back" @click='prevPage' :disabled="pageNumber== 0">Назад</button>
-			<button class="next" @click='nextPage' :disabled="pageNumber >= pageCount -1">Далее</button>
-		</div>
-	</div>
+  <div id="main">
+    <div class="items" ref="items">
+      <div v-if="!getRates" class="lds-dual-ring"></div>
+      <div v-else class="item" v-for="curName of paginatedData">
+        <p class="curName">
+          {{ value ? value : 0 }} <span>{{ baseCurrency }} = </span>
+        </p>
+        <div class="curValue">
+          <span class="valueOfCur">
+            {{
+              calc(getRates[curName]).replace(/\./g, ",") !== "NaN"
+                ? calc(getRates[curName]).replace(/\./g, ",")
+                : "0"
+            }}
+          </span>
+          <span class="curName">
+            {{ curName }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="btn">
+      <button class="back" @click="prevPage" :disabled="pageNumber == 0">
+        Назад
+      </button>
+      <button
+        class="next"
+        @click="nextPage"
+        :disabled="pageNumber >= pageCount - 1"
+      >
+        Далее
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+const BigNumber = require("bignumber.js");
 
-const BigNumber = require('bignumber.js');
-
-export default{
-	props: [
-		'value',
-		'size'
-	],
-	computed: {
-		getRates(){
-			return this.$store.getters.getRates
-		},
-		getListOfRates(){
-			return this.$store.getters.getListOfRates
-		},
-		baseCurrency(){
-			return this.$store.getters.getBaseCurrency
-		},
-		baseSize(){
-			return this.$store.getters.getNumberOfRates - 1
-		},
-		pageCount(){
-      		let l = this.baseSize,
-          	s = this.size;
-      		return Math.ceil(l/s);
-      		// return Math.floor(l/s);``
-		},
-		paginatedData(){
-    		const start = this.pageNumber * this.size,
-          		  end = start + this.size;
-  			const index = this.getListOfRates.indexOf(this.baseCurrency)
-  			const list = [...this.getListOfRates]
-  			index !== -1 ? list.splice(index,1) : "";
-    		return list.slice(start, end)
-		},
-
-},
-	data(){
-		return{
-			pageNumber: 0,
-		}
-	},
-	methods : {
-	  nextPage(){
-         this.pageNumber++;
-      },
-      prevPage(){
-        this.pageNumber--;
-      },
-      calc(rate){
-			return new BigNumber(rate).times(this.value).toFixed(2)
-		}
-	}
-}
-
+export default {
+  props: ["value", "size"],
+  computed: {
+    getRates() {
+      return this.$store.getters.getRates;
+    },
+    getListOfRates() {
+      return this.$store.getters.getListOfRates;
+    },
+    baseCurrency() {
+      return this.$store.getters.getBaseCurrency;
+    },
+    baseSize() {
+      return this.$store.getters.getNumberOfRates - 1;
+    },
+    pageCount() {
+      let l = this.baseSize,
+        s = this.size;
+      return Math.ceil(l / s);
+    },
+    paginatedData() {
+      const start = this.pageNumber * this.size,
+        end = start + this.size;
+      const index = this.getListOfRates.indexOf(this.baseCurrency);
+      const list = [...this.getListOfRates];
+      index !== -1 ? list.splice(index, 1) : "";
+      return list.slice(start, end);
+    },
+  },
+  data() {
+    return {
+      pageNumber: 0,
+    };
+  },
+  methods: {
+    nextPage() {
+      this.pageNumber++;
+    },
+    prevPage() {
+      this.pageNumber--;
+    },
+    calc(rate) {
+      return new BigNumber(rate).times(this.value).toFixed(2);
+    },
+  },
+};
 </script>
 
 
 <style lang="scss">
+.items {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: flex-start;
+  .item {
+    padding: 0 0 19px 23px;
+    width: 46%;
+    color: #2b2d33;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    margin: 0 24px 18px 0;
+    box-shadow: 0px 3px 6px rgba(157, 157, 157, 0.16);
+    border-radius: 12px;
+    .curName {
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 21px;
+      color: #2b2d33;
+      span {
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 21px;
+        color: #b9b9b9;
+      }
+    }
+    .curValue {
+      font-size: 48px;
+      line-height: 56px;
+      letter-spacing: 0.0342857px;
+      font-weight: 300;
+    }
+  }
+  .lds-dual-ring {
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    margin: 0 auto;
+  }
+  .lds-dual-ring:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #fff;
+    border-color: red transparent #fff transparent;
+    animation: lds-dual-ring 1.2s linear infinite;
+  }
+  @keyframes lds-dual-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+}
 
-	.items{
-		display: flex;
-		flex-wrap: wrap;
-		align-items: center;
-		justify-content: flex-start;
-		.item{
-			padding: 0 0 19px 23px;
-			width: 46%;
-			color: #2B2D33;
-			display: flex;
-			flex-direction: column;
-			align-items: flex-start;
-			justify-content: center;
-			margin: 0 24px 18px 0;
-			box-shadow: 0px 3px 6px rgba(157, 157, 157, 0.16);
-			border-radius: 12px;
-			.curName{
-				font-weight: normal;
-				font-size: 18px;
-				line-height: 21px;
-				color: #2B2D33;
-				span{
-					font-weight: normal;
-					font-size: 18px;
-					line-height: 21px;
-					color: #B9B9B9;
-				}
-			}
-			.curValue{
-				font-size: 48px;
-				line-height: 56px;
-				letter-spacing: 0.0342857px;
-				font-weight: 300;
-			}
-		}
-		.lds-dual-ring {
-  			display: inline-block;
-  			width: 80px;
-  			height: 80px;
-  			margin: 0 auto;
-		}
-		.lds-dual-ring:after {
-  			content: " ";
-  			display: block;
-  			width: 64px;
-  			height: 64px;
-  			margin: 8px;
-  			border-radius: 50%;
-  			border: 6px solid #fff;
-  			border-color: red transparent #fff transparent;
-  			animation: lds-dual-ring 1.2s linear infinite;
-		}
-		@keyframes lds-dual-ring {
-  			0% {
-    			transform: rotate(0deg);
-  			}
-  			100% {
-   				 transform: rotate(360deg);
-  			}
-		}
+#main.small {
+  .item {
+    width: 85%;
+    margin: 0 auto;
+    margin-bottom: 18px;
+  }
+}
 
-	}
+.btn {
+  position: relative;
+  margin-top: 41px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-	#main.small{
-		.item{
-			width: 85%;
-			margin: 0 auto;
-			margin-bottom: 18px;
-		}
-	}
+  button {
+    text-transform: uppercase;
+    width: 112px;
+    height: 39px;
+    border: none;
+    border-radius: 8px;
+    margin-right: 19px;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 1px;
+    color: #2b2d33;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
 
-	.btn{
-		position: relative;
-		margin-top: 41px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		
-		button{
-			text-transform: uppercase;
-			width: 112px;
-			height: 39px;
-			border: none;
-			border-radius: 8px;
-			margin-right: 19px;
-			font-weight: 300;
-			font-size: 14px;
-			line-height: 16px;
-			letter-spacing: 1px;
-			color: #2B2D33;
-			&:last-child{
-				margin-right: 0;
-			};
-		}
+  .back {
+    background: #efefef;
+    color: #787878;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    &:before {
+      content: "";
+      display: inline-block;
+      width: 8px;
+      height: 8px;
+      border: 1px solid #787878;
+      border: none;
+      border-bottom: 1px solid;
+      border-left: 1px solid;
+      transform: rotate(45deg);
+      top: calc(50% - 4px);
+      left: 0;
+      margin-right: 11px;
+    }
+  }
 
-		.back{
-			background: #EFEFEF;
-			color: #787878;
-			box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-			&:before{
-				content:'';
-				display: inline-block;
-				width: 8px;
-				height: 8px;
-				border: 1px solid #787878;
-				border: none;
-				border-bottom: 1px solid;
-				border-left: 1px solid;
-				transform: rotate(45deg);
-				top: calc(50% - 4px);
-				left: 0;
-				margin-right: 11px;
-			}
-		}
-
-		.next{
-			background: #FFFFFF;
-			box-shadow: 0 5px 10px rgba(157, 157, 157, 0.16);
-			&:after{
-				content:'';
-				position: absolute;
-				display: inline-block;
-				width: 8px;
-				height: 8px;
-				border: 1px solid #282828;
-				border: none;
-				border-bottom: 1px solid;
-				border-left: 1px solid;
-				transform: rotate(-135deg);
-				top: calc(50% - 4px);
-				margin-left: 11px;
-			}
-		}
-	}
+  .next {
+    background: #ffffff;
+    box-shadow: 0 5px 10px rgba(157, 157, 157, 0.16);
+    &:after {
+      content: "";
+      position: absolute;
+      display: inline-block;
+      width: 8px;
+      height: 8px;
+      border: 1px solid #282828;
+      border: none;
+      border-bottom: 1px solid;
+      border-left: 1px solid;
+      transform: rotate(-135deg);
+      top: calc(50% - 4px);
+      margin-left: 11px;
+    }
+  }
+}
 </style>
